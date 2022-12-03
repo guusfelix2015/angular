da9b8fd2f536442012da155496af8c2c10449040
guusfelix2015 <gusfelix2015@gmail.com>
2022-12-02 22:11:23 -0300

    Estudos angular

---
+++ primeiro-projeto/src/app/app.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+
+import { AppComponent } from './app.component';
+import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component';
+import { CursosModule } from './cursos/cursos.module';
+
+@NgModule({
+  declarations: [AppComponent, MeuPrimeiro2Component],
+  imports: [BrowserModule, AppRoutingModule, CursosModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
