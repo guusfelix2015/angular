50c479277806e94a4815b4f8f1b5f95623001ada
guusfelix2015 <gusfelix2015@gmail.com>
2022-12-03 17:00:22 -0300

    Event binding

--- data-binding/src/app/data-binding/data-binding.component.ts
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -12,12 +12,32 @@ export class DataBindingComponent {
   ulrImage: string =
     'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg';
 
+  valorAtual: string = '';
+  valorSalvo: any;
+  isMouseOver: boolean = false;
+
   getValue() {
     return 1;
   }
 
   getCurtirCurso() {
     return true;
+  }
+
+  botaoClicado() {
+    alert('Botão clicado');
+  }
+
+  OnKeyUp(evento: KeyboardEvent) {
+    this.valorAtual = (<HTMLInputElement>evento.target).value;
+  }
+
+  onMouseOverOut() {
+    this.isMouseOver = !this.isMouseOver;
+  }
+
+  salvarValor(valor: any) {
+    this.valorSalvo = valor;
   }
 
   constructor() {}
