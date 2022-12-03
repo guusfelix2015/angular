787dcebd0ac365b6406058c48717b5ccfe0627d6
guusfelix2015 <gusfelix2015@gmail.com>
2022-12-02 22:50:32 -0300

    Introdução aos serviços

--- primeiro-projeto/src/app/cursos/cursos.component.ts
+++ primeiro-projeto/src/app/cursos/cursos.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CursosService } from './cursos.service';
 
 @Component({
   selector: 'app-cursos',
   templateUrl: './cursos.component.html',
-  styleUrls: ['./cursos.component.css']
+  styleUrls: ['./cursos.component.css'],
 })
-export class CursosComponent {
+export class CursosComponent implements OnInit {
+  nomePortal: string;
 
+  cursos: string[] | undefined;
+
+  constructor(private cursosService: CursosService) {
+    this.nomePortal = 'http://loiane.training';
+
+    this.cursos = this.cursosService.getCursos();
+  }
+
+  ngOnInit() {}
 }
