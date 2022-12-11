7bb8e35c340aa3fe53072137f983e17b5f6fdf19
guusfelix2015 <gusfelix2015@gmail.com>
2022-12-10 22:16:49 -0300

    Interpolação de variáveis

--- horaDeCodar/curso-angular/src/app/components/first-component/first-component.component.ts
+++ horaDeCodar/curso-angular/src/app/components/first-component/first-component.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-first-component',
   templateUrl: './first-component.component.html',
-  styleUrls: ['./first-component.component.scss']
+  styleUrls: ['./first-component.component.scss'],
 })
 export class FirstComponentComponent {
-
+  name: string = 'Gustavo';
+  age: number = 27;
+  job: string = 'Motorista';
 }
