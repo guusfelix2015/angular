49a1f0f600e409610baeca9c82cfe9d1cdd5ef80
guusfelix2015 <gusfelix2015@gmail.com>
2022-12-18 19:03:04 -0300

    inserindo dados no sistema

--- horaDeCodar/moments/src/app/Moment.ts
+++ horaDeCodar/moments/src/app/Moment.ts
@@ -1,8 +1,8 @@
 export interface Moment {
   id?: number;
   title: string;
-  descriptiom: string;
-  image: string;
+  description: string;
+  image?: string;
   created_at?: string;
   updated_at?: string;
   comments?: [{ text: string; username: string }];
--- horaDeCodar/moments/src/app/services/moments.service.ts
+++ horaDeCodar/moments/src/app/services/moments.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { Moment } from '../Moment';
+
+import { environment } from 'src/environment/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MomentsService {
+  private baseApiUrl = environment.baseApiUrl;
+  private apiUrl = `${this.baseApiUrl}api/moments`;
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  createMoment(formData: FormData): Observable<FormData> {
+    return this.http.post<FormData>(this.apiUrl, formData );
+  }
 }
