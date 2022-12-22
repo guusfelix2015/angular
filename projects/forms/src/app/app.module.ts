8bf86ec4789c0809279367bae8d60ebfe857f0f3
guusfelix2015 <gusfelix2015@gmail.com>
2022-12-22 20:26:19 -0300

    Forms reativos

--- projects/forms/src/app/app.module.ts
+++ projects/forms/src/app/app.module.ts
@@ -2,13 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DataFormComponent } from './data-form/data-form.component';
+
 import { FormsModule } from '@angular/forms';
 import { TemplateFormModule } from './template-form/template-form.module';
+import { DataFormModule } from './data-form/data-form.module';
 
 @NgModule({
-  declarations: [AppComponent, DataFormComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule, TemplateFormModule],
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    TemplateFormModule,
+    DataFormModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- projects/forms/src/app/data-form/data-form.component.ts
+++ projects/forms/src/app/data-form/data-form.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-data-form',
   templateUrl: './data-form.component.html',
-  styleUrls: ['./data-form.component.scss']
+  styleUrls: ['./data-form.component.scss'],
 })
 export class DataFormComponent implements OnInit {
+  formulario!: FormGroup;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
